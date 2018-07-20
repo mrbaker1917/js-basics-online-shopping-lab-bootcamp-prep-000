a525252803669a3ad15cfa48cc49869e78897881
Mark Baker <mrbaker1917@gmail.com>
2018-07-20 15:12:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function viewCart() {
   }
   for (var i = 0; i < cart.length; i++) {
     let arr = [];
+    arr.push(cart[i].itemName, cart[i].itemPrice);
     return (`In your cart, you have `);
   }
 }
